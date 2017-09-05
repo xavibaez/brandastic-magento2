5fbd31de8029c2d1c27440f44d23ecf642ac0fee
Xavier Baez <xavierbaez@gmail.com>
2017-09-05 15:25:48 -0700

    auto-add to cart by Xavier Baez

---
+++ code/app/code/Brandastic/AutoCart/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * User: Xavier Baez
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Brandastic_AutoCart',
+    __DIR__
+);
